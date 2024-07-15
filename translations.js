4035a2c7967fbd0645d8354216b7fab96eb1fcc1
designos <112093794+designos@users.noreply.github.com>
2024-07-15 15:43:18 +0300

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,147 @@
+const translations = {
+    en: {
+        activity: "Activity",
+        vision: "Vision",
+        whyUs: "Why us",
+        contactUs: "Contact us",
+        english: "English",
+        arabic: "Arabic",
+        establishment: "Leading Innovation in the Bitumen Industry",
+        establishmentDesc: "At Asmran Company, we work on developing advanced technologies to ensure product quality and sustainability",
+        industry: "Partnership for a Sustainable Future",
+        industryDesc: "We strive to enhance our partnerships to provide effective and environmentally friendly solutions in the field of bitumen",
+        globalPresence: "Our Commitment to Quality and Excellence",
+        globalPresenceDesc: "We are committed to providing the best products and services to meet our customers' needs with the highest standards of quality",
+        aboutCompany: "Company overview",
+        aboutCompanyDesc: "Asmaran Bitumen Manufacturing Company is a leading company in the production and marketing of bitumen products, distinguished in this field. The company was established with the aim of providing innovative and effective solutions for infrastructure and roads, with a commitment to the highest standards of quality and sustainability. Asmran relies on advanced technologies and specialized teams to meet the needs of the local and global market, always striving to enhance its partnerships and achieve customer satisfaction through high-quality products and excellent services",
+        ProductsAndServices: "Manufacturing and Supply of Bitumen",
+        ProductsAndServicesDesc: "Asmaran Company is committed to providing high-quality bitumen to support various infrastructure projects. By utilizing the latest technologies and global quality standards, we ensure the provision of products that contribute to the development of roads, airports, and industrial facilities, enhancing the sustainability and efficiency of these projects",
+        QualityAndCommitment: "Production and Distribution",
+        QualityAndCommitmentDesc: "Asmaran Company produces a wide range of petroleum derivatives that meet both local and global market requirements. Thanks to our extensive distribution network and specialized team, we strive to provide innovative and effective solutions to meet our customers' needs across various sectors, while adhering to environmental and health standards",
+        vision: "Vision",
+        visionDesc: "Asmaran Company aims to achieve excellence and leadership in the field of bitumen and petroleum derivatives manufacturing. Through our commitment to quality, innovation, and sustainability, we strive to meet our customers' needs and create added value in both local and global market",
+        ClientsAndPartners: "Excellence and Innovation",
+        ClientsAndPartnersDesc: "We believe that sustainable success is achieved through innovative and efficient solutions. We invest in research, development, and technology to enhance our operations and ensure the highest quality of our products, efficiently meeting the requirements of infrastructure, transportation, and energy projects",
+        SustainabilityAndEnvironment: "Sustainability and the environment",
+        SustainabilityAndEnvironmentDesc: "At Asmaran Company, we are committed to environmental sustainability and social responsibility. We build sustainable relationships with our partners and clients through transparency and integrity, enhancing our position in the markets and contributing to sustainable economic development",
+        WhyWeAreTheBest: "Why we are the best",
+        wideSelection: "High Quality",
+        wideSelectionDesc: "At Asmaran Company, we ensure the supply of asphalt and petroleum derivatives of unparalleled quality, utilizing the latest technologies and global standards",
+        UnmatchedQuality: "Continuous innovation",
+        UnmatchedQualityDesc: "We continuously invest in research and development to improve our operations and deliver innovative products that meet our customers' aspirations",
+        OutstandingCustomerService: "Sustainable relationships",
+        OutstandingCustomerServiceDesc: "We care about customer satisfaction and always strive to build relationships based on trust and transparency with our partners and clients, helping us achieve our goals successfully together",
+        CommitmentToSustainability: "Sustainability and responsibility",
+        CommitmentToSustainabilityDesc: "We adhere to the highest standards in environmental sustainability and social responsibility, contributing to building a better future for the communities we serve",
+        costumers: "We prioritize customer satisfaction by offering high-quality products and excellent services that meet and exceed their expectations. Choosing Asmaran for asphalt and petroleum derivatives means choosing quality, reliability, and innovation. We are here to be your strategic partner in achieving success and excellence in your projects",
+        conclusion: "Conclusion",
+        conclusionDesc: "At Asmaran Company, we take pride in our commitment to quality, innovation, and sustainability. We always strive to achieve the highest levels of satisfaction for our customers and partners, and look forward to sustainable growth and development in the asphalt and petroleum derivatives industry. Thank you for your trust in us, and we look forward to enhancing our partnership in the future",
+        contactUs: "Contact us",
+        footerDesc: "Asmaran Asphalt Manufacturing Company is a leader in the production and marketing of asphalt products. We are committed to providing innovative solutions for infrastructure and roadways while adhering to the highest standards of quality and sustainability. We offer high-quality products and exceptional services to meet the needs of our customers in both local and global markets",
+        copyRights: "Copyright © ASMARANCO - All rights reserved - 2024",
+    },
+    ar: {
+        activity: "النشاط",
+        vision: "الرؤية",
+        whyUs: "لماذا نحن الأفضل",
+        contactUs: "تواصل معنا",
+        english: "الإنجليزية",
+        arabic: "العربية",
+        establishment: "قيادة الابتكار في صناعة البيتومين",
+        establishmentDesc: "نعمل في شركة اسمران على تطوير تقنيات متقدمة لضمان جودة المنتجات واستدامتها",
+        industry: "شراكة من أجل مستقبل مستدام",
+        industryDesc: "نسعى لتعزيز شراكاتنا لتقديم حلول فعالة وصديقة للبيئة في مجال البيتومين",
+        globalPresenceDesc: "نلتزم بتقديم أفضل المنتجات والخدمات لتلبية احتياجات عملائنا بأعلى معايير الجودة",
+        globalPresence: "التزامنا بالجودة والتميز",
+        aboutCompany: "نبذة عن الشركة",
+        aboutCompanyDesc: "شركة اسمران لصناعة البيتومين هي شركة رائدة في مجال تصنيع وتسويق منتجات البيتومين، وتعتبر من الشركات المتميزة في هذا المجال. تأسست الشركة بهدف تقديم حلول مبتكرة وفعالة للبنية التحتية والطرق، مع الالتزام بأعلى معايير الجودة والاستدامة. تعتمد شركة اسمران على تقنيات متقدمة وفرق عمل متخصصة لتلبية احتياجات السوق المحلي والعالمي، وتسعى دائماً لتعزيز شراكاتها وتحقيق رضا عملائها من خلال منتجات ذات جودة عالية وخدمات متميزة",
+        ProductsAndServices: "تصنيع وتوريد البيتومين",
+        ProductsAndServicesDesc: "تلتزم شركة اسمران بتوفير بيتومين عالي الجودة لدعم مشاريع البنية التحتية المختلفة. من خلال استخدام أحدث التقنيات ومعايير الجودة العالمية، نضمن توفير منتجات تساهم في تطوير الطرق، والمطارات، والمنشآت الصناعية، مما يعزز من استدامة وكفاءة هذه المشاريع",
+        QualityAndCommitment: "الإنتاج والتوزيع",
+        QualityAndCommitmentDesc: "تقوم شركة اسمران بإنتاج مجموعة واسعة من المشتقات النفطية التي تلبي متطلبات السوق المحلي والعالمي. بفضل شبكتنا الواسعة من التوزيع وفريقنا المتخصص، نحرص على تقديم حلول مبتكرة وفعالة لتلبية احتياجات عملائنا في مختلف القطاعات، مع الالتزام بالمعايير البيئية والصحية",
+        vision: "الــرؤية",
+        visionDesc: "تسعى شركة اسمران لصناعة البيتومين والمشتقات النفطية إلى تحقيق التميز والريادة في مجال صناعة البيتومين والمشتقات النفطية. من خلال التزامنا بالجودة والابتكار والاستدامة، نعمل على تلبية احتياجات عملائنا وتحقيق قيمة مضافة في الأسواق المحلية والعالمية",
+        ClientsAndPartners: "التميز والابتكار",
+        ClientsAndPartnersDesc: "نؤمن بأن النجاح المستدام يتحقق عبر حلول مبتكرة وفعالة. نستثمر في البحث والتطوير والتكنولوجيا لتعزيز عملياتنا وضمان أعلى جودة لمنتجاتنا، لتلبية متطلبات مشاريع البنية التحتية والنقل والطاقة بكفاءة",
+        SustainabilityAndEnvironment: "الاستدامة والبيئة",
+        SustainabilityAndEnvironmentDesc: "نحن في شركة اسمران نلتزم بالاستدامة البيئية والمسؤولية الاجتماعية، ونبني علاقات مستدامة مع شركائنا وعملائنا بشفافية ونزاهة، لتعزيز مكانتنا في الأسواق والمساهمة في التنمية الاقتصادية المستدامة",
+        WhyWeAreTheBest: "لماذا نحن الأفضل",
+        wideSelection: "جودة عالية",
+        wideSelectionDesc: "نحن في شركة اسمران نضمن توفير بيتومين ومشتقات نفطية بجودة لا تضاهى، باستخدام أحدث التقنيات والمعايير العالمية",
+        UnmatchedQuality: "ابتكار مستمر",
+        UnmatchedQualityDesc: "نحن نستثمر باستمرار في البحث والتطوير لتحسين عملياتنا وتقديم منتجات مبتكرة تلبي تطلعات عملائنا",
+        OutstandingCustomerService: "علاقات مستدامة",
+        OutstandingCustomerServiceDesc: "نهتم برضا عملائنا ونسعى دائمًا نحن نبني علاقات قائمة على الثقة والشفافية مع شركائنا وعملائنا، مما يساعدنا على تحقيق أهدافنا بنجاح مشترك",
+        CommitmentToSustainability: "استدامة ومسؤولية",
+        CommitmentToSustainabilityDesc: "نحن نلتزم بأعلى المعايير في الاستدامة البيئية والمسؤولية الاجتماعية، مساهمين في بناء مستقبل أفضل للمجتمعات التي نخدمها",
+        costumers: "نضع رضا العملاء في مقدمة أولوياتنا، من خلال تقديم منتجات عالية الجودة وخدمات ممتازة تلبي توقعاتهم وتفوقها اختيارك لشركة أسمران لتصنيع البيتومين ومشتقات النفط يعني أنك تختار الجودة، الموثوقية، والابتكار. نحن هنا لنكون شريكك الاستراتيجي في تحقيق النجاح والتفوق في مشاريعك",
+        conclusion: "ختاماً",
+        conclusionDesc: "نحن في شركة اسمران نفخر بالتزامنا بالجودة والابتكار والاستدامة. نسعى دائماً لتحقيق أعلى مستويات الرضا لعملائنا وشركائنا، ونتطلع إلى النمو والتطور المستدام في صناعة البيتومين والمشتقات النفطية. شكراً لثقتكم بنا ونتطلع إلى تعزيز شراكتنا في المستقبل",
+        contactUs: "تواصل معنا",
+        footerDesc: "شركة اسمران لصناعة البيتومين رائدة في تصنيع وتسويق منتجات البيتومين. نلتزم بتقديم حلول مبتكرة للبنية التحتية والطرق مع مراعاة أعلى معايير الجودة والاستدامة. نقدم منتجات عالية الجودة وخدمات متميزة لتلبية احتياجات عملائنا في السوق المحلي والعالمي",
+        copyRights: "حقوق النشر © جميع الحقوق محفوظة لدى اسمران - 2024",
+    }
+};
+
+const languageSelector = document.querySelector("select")
+
+languageSelector.addEventListener("change", (event) => {
+    setLanguage(event.target.value)
+    localStorage.setItem("language", event.target.value)
+    if (event.target.value === 'ar') {
+        col2.style.textAlign = "right";
+        ProductsAndServices.style.textAlign = "right";
+        ProductsAndServicesDesc.style.textAlign = "right";
+        QualityAndCommitment.style.textAlign = "right";
+        QualityAndCommitmentDesc.style.textAlign = "right";
+        footerDesc.style.textAlign = "right";
+    } else {
+        col2.style.textAlign = "left";
+        ProductsAndServices.style.textAlign = "left";
+        ProductsAndServicesDesc.style.textAlign = "left";
+        QualityAndCommitment.style.textAlign = "left";
+        QualityAndCommitmentDesc.style.textAlign = "left";
+        footerDesc.style.textAlign = "left";
+    }
+});
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     setLanguage(localStorage.getItem("language"));
+// });
+
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll("[data-i18n]")
+    elements.forEach((element) => {
+        const translationKey = element.getAttribute('data-i18n')
+        element.textContent = translations[language][translationKey]
+    });
+    document.dir = language === 'ar' ? 'rtl' : 'ltr';
+};
+
+landingPagePhotoOne = document.querySelector("#landing-page-photo-1");
+landingPagePhotoTwo = document.querySelector("#landing-page-photo-2");
+landingPagePhotoThree = document.querySelector("#landing-page-photo-3");
+ProductsAndServices = document.querySelector(".ProductsAndServices");
+ProductsAndServicesDesc = document.querySelector(".ProductsAndServicesDesc");
+QualityAndCommitment = document.querySelector(".QualityAndCommitment");
+QualityAndCommitmentDesc = document.querySelector(".QualityAndCommitmentDesc");
+footerDesc = document.querySelector("#footerDesc");
+col2 = document.getElementById("col2");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
